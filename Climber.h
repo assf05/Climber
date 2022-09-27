a51176baec1dde281fd19a3f41b829a7e8326b5c
Volcania123 <114417451+Volcania123@users.noreply.github.com>
2022-09-27 15:11:45 +0300

    Update 0.0.1
    Updated .h file with the following actions: 
    GetYaw() 
    GyroYawCalibration()

--- Climber.h
+++ Climber.h
@@ -1,3 +1,8 @@
+/* h code for Climber library
+v0.0.1
+by Ido Azran & Assf Saces
+2022
+*/
 #ifndef CLIMBER_H
 #define CLIMBER_H
 
@@ -10,7 +15,11 @@
   public:
     Climber(); 
     void begin();   
-    void move(int left_speed, int right_speed); //move with right and left speed 
+    void GetYaw(); //gets gyro yaw angle 
+    void GyroYawCalibration(); //Calibrats gyro for correct yaw reading
+    void move(int left_speed, int right_speed); //move with right and left speed
+
+
  };
 
 #endif 
