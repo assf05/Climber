94adc8f1f4ff44418e11f00da7146e71f1955725
Volcania123 <114417451+Volcania123@users.noreply.github.com>
2022-11-02 13:55:53 +0200

    Update climber.ino

--- climber.ino
+++ climber.ino
@@ -8,50 +8,27 @@ By: Ido Azran And Assf Saces
 2022
 */
 
-#include "Climber.h"
-
+#include "Climber.h" 
+#include "HardwareSerial.h"
 
 Climber climber; //create Climber object 
 
+ int YAW_MIN_ERROR = -3;
+ int YAW_MAX_ERROR = 3;  
+ 
+ bool isManual = false; 
+ bool isAuto = false; 
+ bool isUpdate = false;
 
-int left_speed = 200; 
-int right_speed = 200; 
-
-final int YAW_MIN_ERROR = -3;
-final int YAW_MAX_ERROR = 3;
-
-float y;
-
+SoftwareSerial BTSerial(2, 3); // RX | TX 
 
 void setup() {
-  			
   climber.begin(); 
   climber.GyroYawCalibration();
-  
 }
 
 void loop() {  
 
-  y = climber.GetYaw();
 
-  if(y > YAW_MIN_ERROR && y < YAW_MAX_ERROR){ // robot in exceptable range, robot driving stright
-  left_speed = 200; 
-  right_speed = 200; 
-  climber.move(left_speed, right_speed);
-} 
-
-else if (y < YAW_MIN_ERROR) {  // robot to far left
-  left_speed--; 
-  right_speed = 200; 
-  climber.move(left_speed, right_speed);
-} 
-
-else {   // robot to far right
-  left_speed = 200; 
-  right_speed--; 
-  climber.move(left_speed, right_speed);
-}
-	
- climber.cli();
 	
 }
